ce81fec954ae40e670f86208320fd530b1a169a0
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-06-01 22:05:09 +0200

    extends added

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,5 +1,5 @@
 // У вас є функція merge, яка поєднує два об'єкти. Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 
-function merge(objA: object, objB: object) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
