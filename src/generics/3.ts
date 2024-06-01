20f5ac7afb22dc3e170f1ae1091cacf54b2346c3
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-06-01 21:38:25 +0200

    generics

---
+++ src/generics/3.ts
@@ -0,0 +1,5 @@
+// У вас є функція merge, яка поєднує два об'єкти. Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
+
+function merge(objA: object, objB: object) {
+  return Object.assign(objA, objB);
+}
